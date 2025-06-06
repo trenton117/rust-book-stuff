3d35ea16cba3cc90cffa101237dda10b3ba753de
trenton117 <trenton117@protonmail.com>
2025-06-06 00:21:28 -0400

    made ch2 + guessing game crate, and removed cargo stuff from parent dir.

---
+++ ch1/target/debug/deps/ch1-243f82975c48f7d4.d
@@ -0,0 +1,5 @@
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/debug/deps/libch1-243f82975c48f7d4.rmeta: src/main.rs
+
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/debug/deps/ch1-243f82975c48f7d4.d: src/main.rs
+
+src/main.rs:
---
+++ ch1/target/release/deps/ch1-47ba38d90de22a4b.d
@@ -0,0 +1,5 @@
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/release/deps/ch1-47ba38d90de22a4b: src/main.rs
+
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/release/deps/ch1-47ba38d90de22a4b.d: src/main.rs
+
+src/main.rs:
