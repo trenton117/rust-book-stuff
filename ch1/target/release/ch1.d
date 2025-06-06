b016e6735bcce58265465fd4351e17a376cdb325
trenton117 <trenton117@protonmail.com>
2025-06-06 00:31:09 -0400

    fixed .gitignore

--- ch1/target/release/ch1.d
+++
@@ -1,1 +0,0 @@
-/home/trenton/Coding/rust/rust-book-stuff/ch1/target/release/ch1: /home/trenton/Coding/rust/rust-book-stuff/ch1/src/main.rs
--- ch1/target/release/deps/ch1-47ba38d90de22a4b.d
+++
@@ -1,5 +0,0 @@
-/home/trenton/Coding/rust/rust-book-stuff/ch1/target/release/deps/ch1-47ba38d90de22a4b: src/main.rs
-
-/home/trenton/Coding/rust/rust-book-stuff/ch1/target/release/deps/ch1-47ba38d90de22a4b.d: src/main.rs
-
-src/main.rs:
