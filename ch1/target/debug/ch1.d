35c28a0204f87e9cd3c848ea6d1e2230ea3076ec
trenton117 <trenton117@protonmail.com>
2025-06-06 00:02:46 -0400

    made chapter 1 crate

---
+++ ch1/target/debug/ch1.d
@@ -0,0 +1,1 @@
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/debug/ch1: /home/trenton/Coding/rust/rust-book-stuff/ch1/src/main.rs
---
+++ ch1/target/debug/deps/ch1-17faab7abbabd277.d
@@ -0,0 +1,5 @@
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/debug/deps/ch1-17faab7abbabd277: src/main.rs
+
+/home/trenton/Coding/rust/rust-book-stuff/ch1/target/debug/deps/ch1-17faab7abbabd277.d: src/main.rs
+
+src/main.rs:
