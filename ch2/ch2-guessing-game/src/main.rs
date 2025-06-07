15a4ce14ab8602e46d356d87d8a7743de96a25fe
trenton117 <trenton117@protonmail.com>
2025-06-07 03:17:10 -0400

    added rand crate. continue at 2-4

--- ch2/ch2-guessing-game/src/main.rs
+++ ch2/ch2-guessing-game/src/main.rs
@@ -1,13 +1,19 @@
 use std::io; // imports io(input/output) library from std library
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}"); 
 
     println!("Please input your guess.");
 
     let mut guess = String::new(); // create mutable variable 'guess'. it's of type 'String', and it's 'new'.
 
-    io::stdin() // same as 'std::io::stdin', but 'use' on line 1 already called the std library earlier, so we can just use 'io::stdin' now.
+    io::stdin() // same as 'std::io::stdin', but 'use' on line 1 already called the io lib from the std library earlier, so we can just use 'io::stdin' now.
     
         .read_line(&mut guess) // calls the 'read_line' method from the 'stdin' fn. '(&mut guess)'
                                // means it's a mutable reference, rather than immutable
@@ -20,5 +26,8 @@ fn main() {
                                         
     // note that the above method could have been written as one line, but book has me do it in multiple lines for sake of clarity.
 
-    println!("You guess: {guess}");
+    println!("You guessed: {guess}");
 }
+
+// "trivial change"
+// leaving off at Listing 2-4, continue there.
