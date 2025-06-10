d8d7beb8ce67952325e5bad3294e3e52206a05ad
trenton117 <trenton117@protonmail.com>
2025-06-10 03:46:18 -0400

    added match expression w/ cmp function. converted guess string to u32.

--- ch2/ch2-guessing-game/src/main.rs
+++ ch2/ch2-guessing-game/src/main.rs
@@ -1,6 +1,6 @@
+use std::cmp::Ordering; // std library, cmp method, Ordering type
 use std::io; // imports io(input/output) library from std library
-
-use rand::Rng;
+use rand::Rng; // we use the rand crate for the Rng trait for... rng
 
 fn main() {
     println!("Guess the number!");
@@ -26,8 +26,25 @@ fn main() {
                                         
     // note that the above method could have been written as one line, but book has me do it in multiple lines for sake of clarity.
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); 
+    // .trim method removes whitespace, necessary for converting string to u32. 
+    // -- (hitting 'enter' for the input from read_line creates a newline character and must be trimmed. 
+    // ---EXAMPLE: inputting 5 would be "5\n", must trim the "\n".)
+    // .parse method is used to convert a string to another type.
+    // -- .parse returns a Result type, which we use the expect method to handle again.
+    // --- if .parse returns an Err Result variant, it will crash the program with the message provided.
+    // the colon in "guess:" tells rust we'll annotate the variable's type.
+
     println!("You guessed: {guess}");
+    
+    match guess.cmp(&secret_number) { // .cmp method compares two values - in this case, guess and secret_number
+    // match expression tells program what to do based on outcome of the Ordering type returned from cmp method
+        Ordering::Less => println!("Too small!"), 
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    // match expression ends after first successful match, and stops looking further
+    }
 }
 
 // "trivial change"
-// leaving off at Listing 2-4, continue there.
+// continue at multiple guesses w/ looping
